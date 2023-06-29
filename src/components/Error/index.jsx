1d48bebac17e2a93d19c06d36636a1d69be0a1d9
An <anaislcno@gmail.com>
2023-06-29 12:41:08 +0200

    organize files

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,15 @@
+// import Error404 from "../../assets/404.svg"
+// import { Link } from "react-router-dom"
+// import '../Error/Error.scss'
+
+// function Error() {
+//     return (
+//       <div className="error">
+//         <img src={Error404} className="error__img" alt="Logo 404"></img>
+//         <h1 className="error__title">Oups! La page que vous avez demandé n'existe pas.</h1>
+//         <Link className="error__text" to="/">Retourner sur la page d'accueil</Link>
+//       </div>
+//     )
+//   }
+  
+//   export default Error
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,19 @@
+// import { NavLink } from 'react-router-dom'
+// import Logo from '../../assets/logo.svg'
+// import '../Header/Header.scss'
+
+// function Header() {
+//   return (
+//     <header exact activeClassName="active" className='header'>
+//     <NavLink to="/">
+//         <img src={Logo} alt='Logo Kasa' className='header__logo'/>
+//     </NavLink>
+//     <nav className='navbar'>
+//       <NavLink activeClassName="active" className='navbar__link' to="/">Accueil</NavLink>
+//       <NavLink activeClassName="active" className='navbar__link' to="/about">À propos</NavLink>
+//     </nav>
+//     </header>
+//   )
+// }
+
+// export default Header
