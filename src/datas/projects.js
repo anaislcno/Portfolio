e9a51f85fa5ae0f1ce68d985ca5e4b594c9084d8
An <anaislcno@gmail.com>
2023-07-17 16:09:26 +0200

    add responsive and mock-ups on projects section

--- src/datas/projects.js
+++ src/datas/projects.js
@@ -3,8 +3,7 @@ export const projectsList = [
     id: "1",
     title: "Frontend du site Kasa",
     year: "2023",
-    image:
-      "https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "/images/kasa.png",
     description:
       "Projet de formation OpenClassrooms visant à développer le front-end d'un site de location immobilière. Projet nécessitant l'utilisation de composants et de hooks",
     technos: ["React", "Scss"],
@@ -15,8 +14,7 @@ export const projectsList = [
     id: "2",
     title: "Frontend pour Sophie Bluel - Photographe",
     year: "2023",
-    image:
-      "https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "/images/sophie.png",
     description:
       "Projet de formation OpenClassrooms visant à développer le front-end et à dynamiser le site d'une photographe. Implémentation de fonctionnalités (filtres, ajout/suppression de projet, connexion)",
     technos: ["HTML", "CSS", "JavaScript"],
@@ -27,8 +25,7 @@ export const projectsList = [
     id: "3",
     title: "Frontend Booki",
     year: "2023",
-    image:
-      "https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "/images/booki.png",
     description:
       "Projet de formation OpenClassrooms visant à créer le front-end pour la vitrine de la plateforme de voyage Booki",
     technos: ["HTML", "CSS"],
@@ -39,8 +36,7 @@ export const projectsList = [
     id: "4",
     title: "Backend du site Mon Vieux Grimoire",
     year: "2023",
-    image:
-      "https://images.pexels.com/photos/812264/pexels-photo-812264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "/images/grimoire.png",
     description:
       "Projet de formation OpenClassrooms visant à développer le back-end d'un site de notation de livre avec authentification. Construction d'une API, utilisation de la méthode CRUD, utilisaton d'une base de données.",
     technos: ["Node.js", "MongoDB"],
