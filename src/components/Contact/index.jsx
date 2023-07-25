ef5b3882a62ab612d30831651ee55cba1c6987ef
An <anaislcno@gmail.com>
2023-07-25 12:19:04 +0200

    fix details

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import Mail from "../../assets/mail.svg";
 import emailjs from "@emailjs/browser";
 import "../Contact/Contact.scss";
